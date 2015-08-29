b4789865b9299c1410887f6ee30b7b5cf06c3c97
Rolf Eike Beer <eike@sf-mail.de>
2015-08-29 18:15:04 +0200

    add some additional list functions

--- kothic/style/mapcss.js
+++ kothic/style/mapcss.js
@@ -263,6 +263,47 @@ var MapCSS = {
 		return arguments;
 	},
 
+	e_append: function(lst, v) {
+		if (Object.prototype.toString.call(lst) !== '[object Array]')
+			return [];
+
+		lst.push(v);
+
+		return lst;
+	},
+
+	e_join: function(sep, lst) {
+		if (Object.prototype.toString.call(lst) !== '[object Array]')
+			return "";
+
+		return lst.join(sep);
+	},
+
+	e_contains: function(lst, v) {
+		if (Object.prototype.toString.call(lst) !== '[object Array]')
+			return false;
+
+		return (lst.indexOf(v) >= 0);
+	},
+
+	e_sort: function(lst) {
+		if (Object.prototype.toString.call(lst) !== '[object Array]')
+			return [];
+
+		lst.sort();
+
+		return lst;
+	},
+
+	e_reverse: function(lst) {
+		if (Object.prototype.toString.call(lst) !== '[object Array]')
+			return [];
+
+		lst.reverse();
+
+		return lst;
+	},
+
     loadStyle: function (style, restyle, sprite_images, external_images, presence_tags, value_tags) {
         var i;
         sprite_images = sprite_images || [];
